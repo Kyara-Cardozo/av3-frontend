f0edfffe00ccd98593719e1eb78b31dbad309bf6
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2024-10-10 04:37:51 -0300

    Refeita a barra lateral. Ficou melhor, falta o botão pra mudar tema

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+import style from '@/components/css/style.module.css'
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState('light');
+
+  // Carregar o tema salvo no localStorage ou o padrão do sistema
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
+      document.documentElement.classList.add(storedTheme);
+    } else {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+      setTheme(systemTheme);
+      document.documentElement.classList.add(systemTheme);
+    }
+  }, []);
+
+  // Função para alternar o tema
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+    document.documentElement.classList.remove(theme);
+    document.documentElement.classList.add(newTheme);
+  };
+
+  return (
+    <button
+      className={style.themeToggle}
+      onClick={toggleTheme}
+    >
+      {theme === 'light' ? '🌙' : '☀️'}
+    </button>
+  );
+}
--- src/components/data/constants/LinksAdmin.tsx
+++ src/components/data/constants/LinksAdmin.tsx
@@ -1,24 +1,24 @@
-import { Icon, IconArrowLeft, IconBrandTabler, IconChairDirector, IconDashboard, IconLibrary, IconSettings, IconUserBolt, IconUsersGroup } from "@tabler/icons-react";
+import { IconChairDirector, IconHome, IconUsersGroup } from "@tabler/icons-react";
 
 const links = [
     {
       label: "Auditórios",
       href: "#",
       icon: 
-        <IconChairDirector className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconChairDirector size={32} />
     },
     {
       label: "Usuários",
       href: "#",
       icon: 
-        <IconUsersGroup className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconUsersGroup size={32} />
 
     },
     {
       label: "Dashboard",
       href: "#",
       icon: 
-        <IconDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconHome size={32} />
     },
 ];
 
